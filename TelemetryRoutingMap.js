a94adecc339d4ecc8907c383e604f16a94c0bef0
Mario Del Real <madelreal97@gmail.com>
2022-07-05 20:41:00 -0500

    cleaning out js files

--- TelemetryRoutingMap.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const TelemetryRoutingMap = {
-    '/': 'public/index.html',
-    '/basic': 'public/basic.html',
-};
-exports.default = TelemetryRoutingMap;
